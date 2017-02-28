199b3aca2b02035af395faf73b3c2ae2b1b9c8d9
dungvt <dungvt.dev@gmail.com>
2017-02-28 19:33:14 +0700

    add fake data influxdb

--- monitor_admin/mock_data/influxdb_fake_data.py
+++ monitor_admin/mock_data/influxdb_fake_data.py
@@ -1,27 +1,56 @@
 from influxdb import InfluxDBClient
+from threading import Thread
+import random
+import time
 
 host = 'testing'
 port = 8086
 database = 'mydb'
 
-client = InfluxDBClient(host=host, port=port, database=database)
 
-json_body = [
-    {
-        "measurement": "cpu",
-        "tags": {
-            "host": "server01",
-            "region": "us-west"
-        },
-        "time": "2009-11-10T23:00:00Z",
-        "fields": {
-            "value": 0.64
-        }
-    }
-]
+class Mock_thread(Thread):
 
-client.write_points(json_body)
+    def __init__(self, interval_second):
+        super(Mock_thread, self).__init__()
+        self.client = InfluxDBClient(host=host, port=port, database=database)
+        self.interval = interval_second
+        self.stop = False
 
-result = client.query('select value from cpu;');
+    def create_point(self):
+        value = random.random()
+        json_body = [
+            {
+                "measurement": "cpu",
+                "tags": {
+                    "host": "server01",
+                },
+                "fields": {
+                    "value": value
+                }
+            }
+        ]
 
-print("Result: {0}".format(result));
+        return json_body
+
+    def run(self):
+        while not self.stop:
+            js = self.create_point()
+            self.client.write_points(js)
+            # print("WRITE {0}".format(js))
+
+            time.sleep(self.interval)
+
+        print("Thread end")
+
+    def stopThread(self):
+        self.stop = True
+
+
+if __name__ == '__main__':
+    thr = Mock_thread(1)
+    thr.start()
+
+    c = raw_input('Press q to quit')
+
+    if c == 'q':
+        thr.stopThread()
