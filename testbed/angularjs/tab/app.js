eda5b480a5a0ab259e59f0678cbdfda74b4a0824
dungvt <vutrungdungbk@gmail.com>
2017-03-02 19:14:07 +0700

    add testbed angularjs

---
+++ testbed/angularjs/tab/app.js
@@ -0,0 +1,58 @@
+(function(window){
+    angular.module("app", [])
+        .directive('tab', function(){
+            return {
+                restrict: 'E',
+                transclude: true,
+                template: '<h2>Hello world!</h2> <div role="tabpanel" ng-show="active" ng-transclude></div>',
+                require: '^tabset',
+                scope: {
+                    heading: '@'
+                },
+                link: function(scope, elem, attr, tabsetCtrl){
+                    scope.active = false;
+                    scope.disabled = false;
+                    if(attr.disable){
+                        attr.$observe('disable', function(value){
+                            scope.disabled = (value !== 'false')
+                        })
+                    }
+                    tabsetCtrl.addTab(scope);
+                }
+            }
+        })
+        .directive('tabset', function(){
+            return {
+                restrict: 'E',
+                transclude: true,
+                scope: {},
+                templateUrl: 'tabset.html',
+                bindToController: true,
+                controllerAs: 'tabset',
+                controller: function(){
+                    var self = this;
+                    self.tabs = [];
+
+                    self.addTab = function addTab(tab){
+                        self.tabs.push(tab);
+
+                        if(self.tabs.length ===1 ){
+                            tab.active = true;
+                        }
+                    }
+
+                    self.select = function(selectTab){
+                        if(selectTab.disabled) {return}
+
+                        angular.forEach(self.tabs, function(tab){
+                            if(tab.active && tab !== selectTab){
+                                tab.active = false;
+                            }
+                        });
+
+                        selectTab.active = true;
+                    }
+                }
+            }
+        });
+})(window);
